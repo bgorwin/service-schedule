9b709435c65fdf3ce27cb76684ff5c76fe84d8da
Brent Gorwin <brentgorwin@brents-mbp.lan>
2023-06-14 20:19:03 -0500

    Added the CustomerForm component that will add customers to a list with their details.

--- src/app/schedule/page.jsx
+++ src/app/schedule/page.jsx
@@ -1,12 +1,17 @@
 import Navbar from '../../../components/Navbar';
 import Hero from '../../../components/Hero';
 import CustomerList from '../../../components/CustomerList';
+import CustomerForm from '../../../components/CustomerForm';
 
 export default function Schedule() {
     return (
         <div className=" h-screen">
             <Navbar />
             <Hero heading="Schedule Dashboard" />
+
+            <section className="mt-10">
+                <CustomerForm   />
+            </section>
 
             <section className="py-12">
                 <CustomerList />
