9134984c4f68b4b9cc585b625a1bb8db3acebda4
Brent Gorwin <brent.gorwin@gmail.com>
2023-11-04 15:14:43 -0500

    Added Geist font to the project and using it with Tailwind CSS.

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,21 +3,14 @@ import { Montserrat } from '@next/font/google';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
  
-const montserrat = Montserrat({
-  // Specifying weight is only required for
-  // non-variable fonts.
-  weight: '400',
-  subsets: ['latin'],
-  display: 'swap',
-});
-
+import { GeistSans, GeistMono } from 'geist/font'
 export default function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
   children,
 }) {
   return (
-    <html lang="en" className={`${montserrat.className}`}>
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body>
         <Navbar />
         {children}
