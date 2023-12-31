897ec7df7ac9d499ba4d13b0fbed527a1afc8e7b
Brent Gorwin <brent.gorwin@gmail.com>
2023-12-31 13:55:19 -0600

    Working on the About page and added an image and flex layout design. Still working on this and will adjust later.

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
+import Image from 'next/image'
 import Hero from "../../components/Hero";
 
 export default function About () {
     return (
         <div>
             <Hero heading="About Me" image="about-img" buttonText="Back to home page" buttonLink="/" />
-            <div className="container mx-auto flex py-11">
-                <div className="flex flex-row">
-                    <div className="flex-flex-col">
-                        <span className="text-9xl">Hey!</span>
-                        <h2 className="text-7xl">I'm Brent and this is my project</h2>
-                        <p>I have a passion for motorcycles and coding and this is a page where you can utilize your service department and set them up for success. Keep your workorders organized and update the customers on the status of their repairs.</p>
+            <div class="container mx-auto p-4">
+                <div class="flex flex-col md:flex-row lg:flex-row xl:flex-row gap-3">
+                    <div class="flex-1 bg-black rounded-lg p-6">
+                        <h1 className="text-2xl md:text-5xl lg:text-9xl text-white">Hey! <span className="block text-5xl md:text-3xl sm:text-2xl">I'm Brent and this is my project.</span></h1>
+                    </div>
+
+                    <div class="flex-1 bg-orange-500 rounded-lg flex items-center justify-center ps-10 pe-10">
+                        <p className="text-lg">With a passion for motorcycles and Web Development, this program helps with the service department aspect of making sure you provide the best customer experience.</p>
                     </div>
                 </div>
+            </div>
 
+            <div class="container mx-auto p-4">
+                <div class="flex flex-col md:flex-row lg:flex-row xl:flex-row gap-3">
+                    <div class="flex-3 bg-black rounded-lg p-6">
+                        <h1 className="text-2xl md:text-5xl lg:text-9xl text-white">Hey! <span className="block text-5xl md:text-3xl sm:text-2xl">I'm Brent and this is my project.</span></h1>
+                    </div>
+
+                    <div class="flex-2  rounded-lg flex items-center justify-center">
+                        <Image 
+                            src="/images/spencer-davis-HZTveSqEkRQ-unsplash.png"
+                            alt="Motorcycle rider"
+                            width={500}
+                            height={500}
+                            layout="responsive"
+                        />
+                    </div>
+                </div>
             </div>
 
             <div>
