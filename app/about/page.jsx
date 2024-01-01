7a0b2bc9bd03f8544e80c30a6d9fd10d84744389
Brent Gorwin <brent.gorwin@gmail.com>
2024-01-01 17:19:44 -0600

    Working on About page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Image from 'next/image'
-import Hero from "../../components/Hero";
 import Review from '../../components/Review';
 
 
@@ -9,7 +8,7 @@ export default function About () {
     const reviews = [
         {
             id: 1,
-            name: "Jane Doe",
+            name: "Travis Gorwin",
             date: "2021-01-01",
             text: "Great service, highly recommended!",
             rating: 5
@@ -30,7 +29,7 @@ export default function About () {
         },
         {
             id: 4,
-            name: "Harry Potter",
+            name: "Lis Design",
             date: "2022-05-18",
             text: "Great service, highly recommended!",
             rating: 5
@@ -47,14 +46,34 @@ export default function About () {
 
     return (
         <div>
-            <Hero heading="About Me" image="about-img" buttonText="Back to home page" buttonLink="/" />
-            <div class="container mx-auto p-4">
-                <div class="flex flex-col md:flex-row lg:flex-row xl:flex-row gap-3">
-                    <div class="flex-1 bg-black rounded-lg p-6">
+            <div className="flex items-center justify-center h-[80vh] bg-fixed object-fill about-img">
+                {/* Overlay */}
+                <div className="absolute top-0 left-0 right-0 bottom-0 w-3/4 h-[80vh] bg-gradient-to-r from-black to-transparent z-[2]" />
+
+                <div className="flex flex-col md:flex-row lg:flex-row xl:flex-row items-center justify-evenly gap-3 z-10">
+                    <div className="flex-3 rounded-lg p-6">
+                        <h1 className="text-xl md:text-3xl lg:text-7xl animate-left-to-right text-orange-500 text-start">About Us </h1>
+                    </div>
+
+                    <div className="flex-2 rounded-lg flex items-center justify-center">
+                        <Image 
+                            src="/images/spencer-davis-HZTveSqEkRQ-unsplash.png"
+                            alt="Motorcycle rider"
+                            width={500}
+                            height={500}
+                            className="animate-right-to-left"
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div className="container mx-auto pt-10 pb-10">
+                <div className="flex flex-col md:flex-row lg:flex-row xl:flex-row gap-3">
+                    <div className="flex-1 bg-black rounded-lg p-6">
                         <h1 className="text-2xl md:text-5xl lg:text-9xl text-white">Hey! <span className="block text-5xl md:text-3xl sm:text-2xl">I'm Brent and this is my project.</span></h1>
                     </div>
 
-                    <div class="flex-1 bg-orange-500 rounded-lg flex items-center justify-center ps-10 pe-10">
+                    <div className="flex-1 bg-orange-500 rounded-lg flex items-center justify-center ps-10 pe-10">
                         <p className="text-lg">With a passion for motorcycles and Web Development, this program helps with the service department aspect of making sure you provide the best customer experience.</p>
                     </div>
                 </div>
@@ -64,32 +83,14 @@ export default function About () {
                 <p className="text-xl text-center">Find out what our clients and their customers are saying!</p>
                 <div className="container mx-auto flex overflow-x-auto space-x-4 p-4">
                     {reviews.map(review => (
-                        <div key={review.id} className="min-w-max">
+                        <div key={review.id} className="min-w-max"> 
                             <Review {...review} />
                         </div>
                     ))}
                 </div>
-                <p className="text-base">Scroll left to view more</p>
-            </div>
-            
-
-            <div class="container mx-auto p-4">
-                <div class="flex flex-col md:flex-row lg:flex-row xl:flex-row gap-3">
-                    <div class="flex-3 rounded-lg p-6">
-                        <span></span>
-                        <h2 className="text-2xl md:text-5xl lg:text-9xl">Making </h2>
-                    </div>
-
-                    <div class="flex-2  rounded-lg flex items-center justify-center">
-                        <Image 
-                            src="/images/spencer-davis-HZTveSqEkRQ-unsplash.png"
-                            alt="Motorcycle rider"
-                            width={500}
-                            height={500}
-                            layout="responsive"
-                        />
-                    </div>
-                </div>
+                <p className="text-lg text-center">Scroll to view more 
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inline-block"> <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" /></svg>
+                </p>
             </div>
 
             <div>
--- components/Review.jsx
+++ components/Review.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 function Review({ name, date, text, rating }) {
     return (
-        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl border border-orange-300">
             <div className="md:flex">
                 <div className="p-8">
-                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{name}</div>
+                    <div className="uppercase tracking-wide text-sm text-orange-500 font-semibold">{name}</div>
                     <p className="block mt-1 text-lg leading-tight font-medium text-black">{date}</p>
                     <p className="mt-2 text-gray-500">{text}</p>
                     <div className="mt-4">
