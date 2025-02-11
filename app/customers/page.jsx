3cb6bee270bb74da62f019cf51467bc6be04b110
Brent Gorwin <brent.gorwin@gmail.com>
2025-02-11 15:18:03 -0800

    Fixed build fail error

--- app/customers/page.jsx
+++ app/customers/page.jsx
@@ -5,22 +5,22 @@ import { supabase } from '../../utils/supabase/server';
 
 
           
-export default async function Customers() {
-    let { data: workorders, error } = await supabase
-    .from('workorders')
-    .select('*');
-    console.log(workorders)
-    return (
-        <div>
-            <h1>Data from Supabase</h1>
-            <p>{workorders.map((workorder) => {
-                return (
-                    <div key={workorder.id}>
-                        <h2>{workorder.name}</h2>
-                        <p>{workorder.status}</p>
-                    </div>
-                );
-            })}</p>
-        </div>
-    );
-}
+// export default async function Customers() {
+//     let { data: workorders, error } = await supabase
+//     .from('workorders')
+//     .select('*');
+//     console.log(workorders)
+//     return (
+//         <div>
+//             <h1>Data from Supabase</h1>
+//             <p>{workorders.map((workorder) => {
+//                 return (
+//                     <div key={workorder.id}>
+//                         <h2>{workorder.name}</h2>
+//                         <p>{workorder.status}</p>
+//                     </div>
+//                 );
+//             })}</p>
+//         </div>
+//     );
+// }
